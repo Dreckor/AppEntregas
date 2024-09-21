b688371c469ecf5d800b6dd5d30c0d37443eefc3
Dreckor <brayangiovanirico@gmail.com>
2024-09-20 20:19:12 -0500

    deploy 2.0

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,9 +5,12 @@ import { PORT } from "./config.js";
 async function main() {
     try {
       await connectdb();
-      app.listen(PORT);
-      console.log(`Listening on port http://localhost:${PORT}`);
-      //console.log(`Environment: ${process.env.NODE_ENV}`)
+      app.listen(PORT, '0.0.0.0', () => {
+            console.log(`Servidor escuchando en http://localhost:${PORT}`);
+            console.log(`Entorno: ${process.env.NODE_ENV}`);
+        });
+      
+      
     } catch (error) {
       console.error(error);
     }
